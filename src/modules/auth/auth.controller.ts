27ef76b6ce27412aeb21555846e01ae92e46dd84
MohamedOsamaM <MohamedOsama99824@gmail.com>
2025-08-30 04:52:40 +0300

    remove

--- src/modules/auth/auth.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthService from "./auth.service";
-import * as validators from "./auth.validation";
-import { validation } from "../../middleware/validation.middleware";
-const router: Router = Router();
-router.post("/signup", validation(validators.signup), AuthService.signup);
-router.post("/login", AuthService.login);
-
-export default router;
--- src/modules/auth/auth.dto.ts
+++
@@ -1,3 +0,0 @@
-import * as validators from "./auth.validation";
-import { z } from "zod";
-export type ISignupBodyInputsDTO = z.infer<typeof validators.signup.body>;
